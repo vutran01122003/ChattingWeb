ec3c63ad3edbfec8446b1034b35aa00bb5f8a643
Vu Tran <tranducvu234@gmail.com>
2025-04-23 00:18:10 +0700

    update: modify socket initlization

--- src/SocketClient.jsx
+++ src/SocketClient.jsx
@@ -2,12 +2,20 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 import { useDispatch } from "react-redux";
 import { delSocket, getUsersOnline, setSocket } from "./redux/slices/socketSlice";
+import { getAccessToken, getUserCredentials } from "./utils";
 
 function SocketClient({ auth }) {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const socket = io(import.meta.env.VITE_APP_BASE_API_URI);
+        const { accessToken } = getUserCredentials();
+        const socket = io(import.meta.env.VITE_APP_BASE_API_URI, {
+            auth: {
+                userId: auth?.user._id,
+                token: accessToken
+            }
+        });
+
         if (auth?.user) {
             socket.on("connect", () => {
                 dispatch(setSocket(socket));
