136bfd7fdbb81572fa37b4b048f92a0c950e890c
Vu Tran <111957751+vutran01122003@users.noreply.github.com>
2025-05-31 00:43:21 +0700

    Chore: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://13.215.205.24',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+    plugins: [react(), tailwindcss()]
 });
